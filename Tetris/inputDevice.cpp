86b7a7f5544c48a140522f70c434287d0280f04d
Siarlo <simsimtrudeau@gmail.com>
2023-02-22 17:38:33 -0500

    Delay input

--- Tetris/inputDevice.cpp
+++ Tetris/inputDevice.cpp
@@ -1,31 +1,77 @@
 #include "inputDevice.h"
 
+InputDevice::InputDevice() {
+    milliseconds requiredDelayMillis(250);
+    lastDropFaster = high_resolution_clock::now() - requiredDelayMillis;
+    lastDropOnce = high_resolution_clock::now() - requiredDelayMillis;
+    lastRotateLeft = high_resolution_clock::now() - requiredDelayMillis;
+    lastRotateRight = high_resolution_clock::now() - requiredDelayMillis;
+    lastTranslateLeft = high_resolution_clock::now() - requiredDelayMillis;
+    lastTranslateRight = high_resolution_clock::now() - requiredDelayMillis;
+}
+
 bool InputDevice::translateLeft()
 {
-    return KeyboardInput::translateLeft();
+    if (duration_cast<milliseconds>(high_resolution_clock::now() - lastTranslateLeft).count() > 250 && KeyboardInput::translateLeft()) {
+        lastTranslateLeft = high_resolution_clock::now();
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 bool InputDevice::translateRight()
 {
-    return KeyboardInput::translateRight();
+    if (duration_cast<milliseconds>(high_resolution_clock::now() - lastTranslateRight).count() > 250 && KeyboardInput::translateRight()) {
+        lastTranslateRight = high_resolution_clock::now();
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 bool InputDevice::rotateRight()
 {
-    return KeyboardInput::rotateRight();
+    if (duration_cast<milliseconds>(high_resolution_clock::now() - lastRotateRight).count() > 250 && KeyboardInput::rotateRight()) {
+        lastRotateRight = high_resolution_clock::now();
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 bool InputDevice::rotateLeft()
 {
-    return KeyboardInput::rotateLeft();
+    if (duration_cast<milliseconds>(high_resolution_clock::now() - lastRotateLeft).count() > 250 && KeyboardInput::rotateLeft()) {
+        lastRotateLeft = high_resolution_clock::now();
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 bool InputDevice::dropFaster()
 {
-    return KeyboardInput::dropFaster();
+    if (duration_cast<milliseconds>(high_resolution_clock::now() - lastDropFaster).count() > 250 && KeyboardInput::dropFaster()) {
+        lastDropFaster = high_resolution_clock::now();
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 bool InputDevice::dropOnce()
 {
-    return KeyboardInput::dropOnce();
+    if (duration_cast<milliseconds>(high_resolution_clock::now() - lastDropOnce).count() > 250 && KeyboardInput::dropOnce()) {
+        lastDropOnce = high_resolution_clock::now();
+        return true;
+    }
+    else {
+        return false;
+    }
 }
--- Tetris/inputDevice.h
+++ Tetris/inputDevice.h
@@ -1,12 +1,24 @@
 #ifndef INPUT_DEVICE
 #define INPUT_DEVICE
 
+#include <chrono>
 #include "keyboardInput.h"
+
+using namespace std::chrono;
 
 class InputDevice
 {
+private:
+	high_resolution_clock::time_point lastTranslateLeft;
+	high_resolution_clock::time_point lastTranslateRight;
+	high_resolution_clock::time_point lastRotateLeft;
+	high_resolution_clock::time_point lastRotateRight;
+	high_resolution_clock::time_point lastDropFaster;
+	high_resolution_clock::time_point lastDropOnce;
+
 
 public:
+	InputDevice();
 	bool translateLeft();
 	bool translateRight();
 	bool rotateRight();
